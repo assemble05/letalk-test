21058adcaf53a231e5544eeb4e09728f640e2251
assemble05 <joneto18.jh@gmail.com>
2022-09-12 20:11:21 -0300

    input style and validations

---
+++ src/components/input/inputStyle.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  width: 904px;
+  height: 25px;
+  font-size: 18px;
+  padding: 18px 0px 17px 21px;
+  border-radius: 6px;
+  border-color: 1px solid #d4d4d4;
+  :hover {
+    border-color: #f3a126;
+  }
+`;
+export default StyledInput;
---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+export interface IInputProps {
+ placeholder : string;
+ register: any;
+ registerName : string;
+
+}
